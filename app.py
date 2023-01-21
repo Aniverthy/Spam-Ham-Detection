61b7f5a25df51419359499e0a0bf586f6734122f
Aniverthy <97465130+Aniverthy@users.noreply.github.com>
2023-01-21 12:07:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,9 @@ import joblib
 model_nb=joblib.load('spam-ham')
 
 st.title('SPAM HAM CLASSIFIER')
-ip=st.text_input('Enter your text:')
+ip=st.file_uploader("Upload an image:")
+if image:
+    st.image(image, caption="Uploaded image", width=300)
 
 op=model_nb.predict([ip])
 if st.button('PREDICT'):
